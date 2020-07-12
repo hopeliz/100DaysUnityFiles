c86df65bbb245b85ca644ce22ed51a8da2733e1e
Hope Moore <hopemoore@gmail.com>
2020-07-12 00:33:08 -0400

    Add Day 62

---
+++ Assets/Scripts/ChangeLight.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLight : MonoBehaviour
+{
+    public Material redLight;
+    public Material red;
+
+    public bool redLightOn = false;
+    public float redCountdown;
+    public float redLength = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        redCountdown = redLength;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (redLightOn)
+        {
+            redCountdown -= Time.deltaTime;
+
+            if (redCountdown <= 0)
+            {
+                GetComponent<Renderer>().material = red;
+                redLightOn = false;
+                redCountdown = redLength;
+            }
+        }
+
+        if (!redLightOn)
+        {
+            redCountdown -= Time.deltaTime;
+
+            if (redCountdown <= 0)
+            {
+                GetComponent<Renderer>().material = redLight;
+                redLightOn = true;
+                redCountdown = redLength;
+            }
+        }
+
+        
+    }
+}
